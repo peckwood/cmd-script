f6ed413bdf45bd467dad56479ba322760223e71a
fengxiao <fengxiao@sxylzinfo.com>
2022-10-22 19:13:17 +0800

    added start-idea.cmd

---
+++ start-idea.cmd
@@ -0,0 +1,5 @@
+rem this is used in spellbook
+
+PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "C:\Users\raiden\StartupLog.txt" 2>&1
+
+PowerShell C:\Users\raiden\project\powershell_scripts\start-idea.ps1 >> C:\Users\raiden\StartupLog.txt 2>&1
